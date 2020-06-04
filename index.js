df19c16d5b35e2b410b2bb1347aaea82480c8f2a
Devo <the.devin.carter@outlook.com>
2020-06-04 09:27:37 -0500

    working call to MailChimp API

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('superagent')
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var mailchimpInstance = 'us10'
+var listUniqueId = 'dd6fe0783f'
+var mailchimpApiKey = '22b70c35c161c5b27f5d6db2d1869aa0-us10'
+
+app.get("/", (req, res) => {
+  console.log("Hello World");
+});
+
+app.get('/members', (req, res) => {
+  request
+    .get('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members')
+    .auth('owner', mailchimpApiKey)
+    .then(res => {
+      let textObj = JSON.parse(res.res.text)
+      console.log('Total New Members: ', textObj.members.length);
+      // console.log('Members: ', textObj.members);
+
+      textObj.members.forEach(member => {
+        let id = member.id
+        let email = member.email_address
+        //! The following MUST MATCH the merge field names from the given form
+        // let name = member.merge_fields.<field_name>
+        // let businessName = member.merge_fields.<field_name>
+        // let category = member.merge_fields.<field_name>
+        // let location = member.merge_fields.<field_name>
+        // let donationSite = member.merge_fields.<field_name>
+        // let website = member.merge_fields.<field_name>
+
+        //! For testing only
+        let name = member.merge_fields.NAME
+        let category = member.merge_fields.MMERGE2
+        let location = member.merge_fields.MMERGE3
+        let website = member.merge_fields.MMERGE4
+
+        console.log({ 'Name': name, 'Category': category, 'Location': location, 'Website': website });
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000')
+})
